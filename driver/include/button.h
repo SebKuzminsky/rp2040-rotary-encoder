baa40599a3c0a1c468bd91793f1a816628c6715d
Sebastian Kuzminsky <seb@highlab.com>
2023-10-20 21:48:24 -0600

    switch button driver to pio
    This PIO program scans a GPIO input about once every 500 us (2 kHz).
    If there are 32 identical consecutive readings, it reports a button
    transition (pressed to not-pressed, or vise versa).

--- driver/include/button.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BUTTON_H__
-#define __BUTTON_H__
-
-void button_init(void);
-bool button_scan(void);
-
-#endif // __BUTTON_H__
--- example/main.cpp
+++ example/main.cpp
@@ -4,7 +4,14 @@
 #include <pico/stdlib.h>
 
 #include "encoder.h"
-#include "button.h"
+
+
+#define DO_BUTTON 1
+
+
+#if DO_BUTTON
+#include "button.pio.h"
+#endif
 
 
 int main() {
@@ -13,22 +20,39 @@ int main() {
     printf("booted\n");
 
     encoder_init();
-    button_init();
 
-    int r;
+    PIO pio = pio0;
+
+#if DO_BUTTON
+    uint button_offset = 0;
+
+    if (pio_can_add_program_at_offset(pio, &button_program, button_offset)) {
+        printf("adding button at %u\n", button_offset);
+    } else {
+        printf("can't add button at %u\n", button_offset);
+    }
+
+    pio_add_program_at_offset(pio, &button_program, button_offset);
+    printf("loaded button at 0x%08x\n", button_offset);
+
+    uint const button_gpio = 2;
+
+    button_init(pio, button_offset, button_gpio);
+#endif
 
     for (;;) {
-        r = encoder_scan();
+        int r = encoder_scan();
         if (r == 1) {
             printf("cw\n");
         } else if (r == -1) {
             printf("ccw\n");
         }
 
-        r = button_scan();
-        if (r == 1) {
-            // click
-            printf("click!\n");
+#if DO_BUTTON
+        uint32_t button_state;
+        if (button_get_state(button_state)) {
+            printf("got a button edge: 0x%08lx\n", button_state);
         }
+#endif
     }
 }
