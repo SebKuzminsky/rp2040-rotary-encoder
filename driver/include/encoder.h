826f051cb62e0ca2ba008ce4c58acec906d364b5
Sebastian Kuzminsky <seb@highlab.com>
2023-11-15 22:22:21 -0700

    switch encoder to use quadrature_encoder.pio
    This runs the quadrature_encoder and the button debouncer on different
    PIO instances.
    
    This is required so that the slow clock of the debounce doesn't mess up
    the quadrature decoder.
    
    The quadrature program takes up almost all the instruction memory of the
    PIO instance it's running on, so there's not room to add a bunch of NOPs
    to the debouncer to slow it down.

--- driver/include/encoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ENCODER_H__
-#define __ENCODER_H__
-
-
-void encoder_init(void);
-int encoder_scan(void);
-
-
-#endif // __ENCODER_H__
--- example/main.cpp
+++ example/main.cpp
@@ -3,15 +3,8 @@
 #include <stdlib.h>
 #include <pico/stdlib.h>
 
-#include "encoder.h"
-
-
-#define DO_BUTTON 1
-
-
-#if DO_BUTTON
+#include "quadrature_encoder.pio.h"
 #include "button.pio.h"
-#endif
 
 
 int main() {
@@ -19,40 +12,45 @@ int main() {
     sleep_ms(3*1000);
     printf("booted\n");
 
-    encoder_init();
+    // Base pin to connect the A phase of the encoder.
+    // The B phase must be connected to the next pin
+    const uint PIN_AB = 0;
 
-    PIO pio = pio0;
+    // we don't really need to keep the offset, as this program must be loaded
+    // at offset 0
+    (void)pio_add_program(pio0, &quadrature_encoder_program);
+    quadrature_encoder_program_init(pio0, PIN_AB, 0);
 
-#if DO_BUTTON
     uint button_offset = 0;
 
-    if (pio_can_add_program_at_offset(pio, &button_program, button_offset)) {
+    if (pio_can_add_program_at_offset(pio1, &button_program, button_offset)) {
         printf("adding button at %u\n", button_offset);
     } else {
         printf("can't add button at %u\n", button_offset);
     }
 
-    pio_add_program_at_offset(pio, &button_program, button_offset);
+    pio_add_program_at_offset(pio1, &button_program, button_offset);
     printf("loaded button at 0x%08x\n", button_offset);
 
     uint const button_gpio = 2;
 
-    button_init(pio, button_offset, button_gpio);
-#endif
+    button_init(pio1, button_offset, button_gpio);
 
+    int old_value = 0;
     for (;;) {
-        int r = encoder_scan();
-        if (r == 1) {
-            printf("cw\n");
-        } else if (r == -1) {
-            printf("ccw\n");
+        int new_value, delta;
+        // note: thanks to two's complement arithmetic delta will always
+        // be correct even when new_value wraps around MAXINT / MININT
+        new_value = quadrature_encoder_get_count();
+        delta = new_value - old_value;
+        old_value = new_value;
+        if (delta != 0) {
+            printf("position %8d, delta %6d\n", new_value, delta);
         }
 
-#if DO_BUTTON
         uint32_t button_state;
         if (button_get_state(button_state)) {
             printf("got a button edge: 0x%08lx\n", button_state);
         }
-#endif
     }
 }
